01176874e898d310a7f7d30e268b22348a0e6b26
Afra Anjum <lilithkard77@gmail.com>
2025-10-14 08:42:20 +0600

    added module 6.5 practice tasks

---
+++ Semester 01/Introduction to Programming/week 02/module 6.5/I_Lucky_Numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int first_digit = n / 10;
+    int second_digit = n % 10;
+ 
+    if (first_digit == 0 || second_digit == 0) {
+        if (first_digit == 0 && second_digit == 0)
+            printf("YES");
+        else if (first_digit != 0 && second_digit == 0) 
+            printf("YES");
+        else
+            printf("NO");
+    }
+    else if (first_digit % second_digit == 0 || second_digit % first_digit == 0) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
+ 
+    return 0;
+}
